1d2b0896a1a66b00765c8281760087ca79124776
Xinben Gao <75108266+ellagao@users.noreply.github.com>
2021-02-08 22:10:45 +0800

    Add files via upload

---
+++ stereo.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from matplotlib import pyplot as plt
+import numpy as np
+import csv
+import re
+import cv2
+
+def read_calib(calib_file_path):
+    with open(calib_file_path, 'r') as calib_file:
+        calib = {}
+        csv_reader = csv.reader(calib_file, delimiter='=')
+        for attr, value in csv_reader:
+            calib.setdefault(attr, value)
+    return calib
+
+def read_pfm(pfm_file_path):
+    with open(pfm_file_path, 'rb') as pfm_file:
+        header = pfm_file.readline().decode().rstrip()
+        channels = 3 if header == 'PF' else 1
+        dim_match = re.match(r'^(\d+)\s(\d+)\s$', pfm_file.readline().decode('utf-8'))
+        if dim_match:
+            width, height = map(int, dim_match.groups())
+        else:
+            raise Exception("Malformed PFM header.")
+
+        scale = float(pfm_file.readline().decode().rstrip())
+        if scale < 0:
+            endian = '<' # littel endian
+            scale = -scale
+        else:
+            endian = '>' # big endian
+
+        dispariy = np.fromfile(pfm_file, endian + 'f')
+    #
+    img = np.reshape(dispariy, newshape=(height, width, channels))
+    img = np.flipud(img).astype('uint8')
+    #
+    plt.imshow( img,cmap='jet' )
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+    return dispariy, [(height, width, channels), scale]
+
+
+def create_depth_map(pfm_file_path, calib=None):
+
+    dispariy, [shape,scale] = read_pfm(pfm_file_path)
+
+    if calib is None:
+        raise Exception("Loss calibration information.")
+    else:
+        fx = float(calib['cam0'].split(' ')[0].lstrip('['))
+        base_line = float(calib['baseline'])
+        doffs = float(calib['doffs'])
+
+		# scale factor is used here
+        depth_map = fx*base_line / (dispariy / scale + doffs)
+        depth_map = np.reshape(depth_map, newshape=shape)
+        depth_map = np.flipud(depth_map).astype('uint8')
+        #depth_map=256-depth_map
+        return depth_map
+
+
+
+
+pfm_file_dir = Path(r'D:\Adirondack-imperfect')
+calib_file_path = pfm_file_dir.joinpath('calib.txt')
+disp_left = pfm_file_dir.joinpath('disp0.pfm')
+	
+# calibration information
+calib = read_calib(calib_file_path)
+# create depth map
+depth_map_left = create_depth_map(disp_left, calib)
+
+plt.imshow( depth_map_left,cmap='gray_r' )
+plt.xticks([])
+plt.yticks([])
+plt.show()
